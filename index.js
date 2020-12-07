a57017fa7245e1cd80d044233737aa0bf08938d5
David Montoya Ramírez <deividelpaisa@hotmail.com>
2020-12-07 10:37:03 -0500

    base de datos agregada

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('./db.json');
-const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3000;
+var jsonServer = require('json-server');
+var db = require('./db.js');
+var fs = require('fs');
+
+var server = jsonServer.create();
+
+fs.writeFileSync('/tmp/db.json', JSON.stringify(db()));
+
+var router = jsonServer.router('./db.json');
+var middlewares = jsonServer.defaults();
+var port = process.env.PORT || 3000;
 
 server.use(middlewares);
 server.use(router);
